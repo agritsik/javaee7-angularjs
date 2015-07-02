55b7d23c364575d1706bbd08f1149f5d30d3c3a5
andrey <a.gritsik@gmail.com>
2015-07-02 13:34:05 +0300

    use maven.test.skip rather than skipTests to avoid test class compilation

--- docker/run-package.sh
+++ docker/run-package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-mvn clean -f catalog/pom.xml && mvn package -f catalog/pom.xml -DskipTests -Pdocker-dev
+mvn clean -f catalog/pom.xml && mvn package -f catalog/pom.xml -Pdocker-dev
 
 cp catalog/target/catalog-1.0-SNAPSHOT.war docker/catalog-gf/app.war
