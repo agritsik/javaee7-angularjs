954ffafd542c7c335ba65e35a2ca7e925c10930a
andrey <a.gritsik@gmail.com>
2015-07-09 12:33:41 +0300

    optimized docker containers and .sh scripts

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# build server
 sh docker/server/run-package.sh
 sh docker/server/run-db.sh
 sh docker/server/run-gf.sh
 
+# build client
 sh docker/client/run-build.sh
 sh docker/client/run-nginx.sh
 
